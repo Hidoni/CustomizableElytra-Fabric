b6acb436d7302b96f1fd7bffaed5f13d0f5996df
Hidoni <ron30086@walla.com>
2021-05-07 16:56:09 +0300

    Added recipe to revert elytra back to vanilla item

--- src/main/java/com/hidoni/customizableelytrafabric/registry/ModRecipes.java
+++ src/main/java/com/hidoni/customizableelytrafabric/registry/ModRecipes.java
@@ -3,6 +3,7 @@ package com.hidoni.customizableelytrafabric.registry;
 import com.hidoni.customizableelytrafabric.CustomizableElytra;
 import com.hidoni.customizableelytrafabric.recipe.ElytraBannerRecipe;
 import com.hidoni.customizableelytrafabric.recipe.ElytraDyeRecipe;
+import com.hidoni.customizableelytrafabric.recipe.ElytraToVanillaRecipe;
 import com.hidoni.customizableelytrafabric.recipe.ElytraWingCombinationRecipe;
 import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.util.Identifier;
@@ -13,6 +14,7 @@ public class ModRecipes
     public static SpecialRecipeSerializer<ElytraDyeRecipe> ELYTRA_DYE_RECIPE = Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(CustomizableElytra.MOD_ID, "elytra_dye_recipe"), new SpecialRecipeSerializer<>(ElytraDyeRecipe::new));
     public static SpecialRecipeSerializer<ElytraBannerRecipe> ELYTRA_BANNER_RECIPE = Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(CustomizableElytra.MOD_ID, "elytra_banner_recipe"), new SpecialRecipeSerializer<>(ElytraBannerRecipe::new));
     public static SpecialRecipeSerializer<ElytraWingCombinationRecipe> ELYTRA_WING_COMBINATION_RECIPE = Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(CustomizableElytra.MOD_ID, "elytra_wing_combination_recipe"), new SpecialRecipeSerializer<>(ElytraWingCombinationRecipe::new));
+    public static SpecialRecipeSerializer<ElytraToVanillaRecipe> ELYTRA_TO_VANILLA_RECIPE = Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(CustomizableElytra.MOD_ID, "elytra_to_vanilla_recipe"), new SpecialRecipeSerializer<>(ElytraToVanillaRecipe::new));
 
     public static void register()
     {

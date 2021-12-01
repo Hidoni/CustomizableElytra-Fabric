b5b53e7c8ab7d1a6bfeb4fc4251162d44cbd5479
Hidoni <ron30086@walla.com>
2021-12-01 22:05:36 +0200

    Split off various calls in CustomizableElytra.onInitializeClient to functions

--- src/main/java/com/hidoni/customizableelytrafabric/client/CustomizableElytra.java
+++ src/main/java/com/hidoni/customizableelytrafabric/client/CustomizableElytra.java
@@ -22,14 +22,25 @@ public class CustomizableElytra implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        registerModelPredicates();
+        registerColorProviders();
+        registerElytraBannerPatterns();
+        registerFeatureRendererEventHandlers();
+    }
+
+    private void registerModelPredicates() {
         FabricModelPredicateProviderRegistry.register(ModItems.CUSTOMIZABLE_ELYTRA, new Identifier(MOD_ID, "broken_elytra"), ((stack, world, entity, seed) ->
                 stack.getItem() == ModItems.CUSTOMIZABLE_ELYTRA && CustomizableElytraItem.isUsable(stack) ? 0.0F : 1.0F));
+    }
 
+    private void registerColorProviders() {
         ColorProviderRegistry.ITEM.register((stack, tintIndex) ->
                 ((CustomizableElytraItem) stack.getItem()).getColor(stack, tintIndex), ModItems.CUSTOMIZABLE_ELYTRA);
         ColorProviderRegistry.ITEM.register((stack, tintIndex) ->
                 tintIndex > 0 ? -1 : ((ElytraWingItem) stack.getItem()).getColor(stack), ModItems.ELYTRA_WING);
+    }
 
+    private void registerElytraBannerPatterns() {
         ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).register(((atlasTexture, registry) ->
         {
             for (BannerPattern pattern : BannerPattern.values()) {
@@ -37,6 +48,9 @@ public class CustomizableElytra implements ClientModInitializer {
                 registry.register(textureIdentifier);
             }
         }));
+    }
+
+    private void registerFeatureRendererEventHandlers() {
         LivingEntityFeatureRenderEvents.ALLOW_CAPE_RENDER.register((entity) -> {
             ItemStack stack = CustomizableElytraFeatureRenderer.tryFindElytra(entity);
             return stack.getItem() == ModItems.CUSTOMIZABLE_ELYTRA;

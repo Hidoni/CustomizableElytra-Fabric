4e0275b59e27319c57bed369be766ba25000d416
Hidoni <ron30086@walla.com>
2021-12-01 21:53:02 +0200

    Migrate TrinketsIntegration to rely on Fabric API for damage

--- src/main/java/com/hidoni/customizableelytrafabric/integration/trinkets/CustomizableElytraTrinket.java
+++ src/main/java/com/hidoni/customizableelytrafabric/integration/trinkets/CustomizableElytraTrinket.java
@@ -1,51 +1,35 @@
 package com.hidoni.customizableelytrafabric.integration.trinkets;
 
-import com.hidoni.customizableelytrafabric.item.CustomizableElytraItem;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
 import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.item.ElytraItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Pair;
 import net.minecraft.world.event.GameEvent;
-import top.theillusivec4.caelus.api.CaelusApi;
-
-import java.util.UUID;
 
 public class CustomizableElytraTrinket implements Trinket {
-    private static final EntityAttributeModifier FLIGHT_MODIFIER = new EntityAttributeModifier(UUID.fromString("a34b6e10-c28d-4a11-b131-ae54b505298b"), "Customizable elytra trinket flight", 1.0D, EntityAttributeModifier.Operation.ADDITION);
+    public static boolean useCustomElytraTrinket(LivingEntity entity, Pair<SlotReference, ItemStack> slotReferenceItemStackPair, boolean tickElytra) {
+        if (ElytraItem.isUsable(slotReferenceItemStackPair.getRight())) {
+            if (tickElytra) {
+                doTrinketElytraTick(entity, slotReferenceItemStackPair.getRight(), slotReferenceItemStackPair.getLeft());
+            }
+            return true;
+        }
 
-    @Override
-    public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        int i = entity.getRoll() + 1;
-        if (!entity.world.isClient && i % 10 == 0) {
-            int j = i / 10;
-            if (j % 2 == 0) {
-                stack.damage(1, entity, (player) ->
-                {
-                    TrinketsApi.onTrinketBroken(stack, slot, entity);
-                });
+        return false;
+    }
+
+    public static void doTrinketElytraTick(LivingEntity entity, ItemStack stack, SlotReference slotReference) {
+        int nextRoll = entity.getRoll() + 1;
+
+        if (!entity.world.isClient && nextRoll % 10 == 0) {
+            if ((nextRoll / 10) % 2 == 0) {
+                stack.damage(1, entity, p -> TrinketsApi.onTrinketBroken(stack, slotReference, entity));
             }
 
             entity.emitGameEvent(GameEvent.ELYTRA_FREE_FALL);
-        }
-
-        EntityAttributeInstance flightAttributeInstance = entity.getAttributeInstance(CaelusApi.getInstance().getFlightAttribute());
-        if (CustomizableElytraItem.isUsable(stack)) {
-            if (flightAttributeInstance != null && !flightAttributeInstance.hasModifier(FLIGHT_MODIFIER)) {
-                flightAttributeInstance.addTemporaryModifier(FLIGHT_MODIFIER);
-            }
-        } else {
-            flightAttributeInstance.removeModifier(FLIGHT_MODIFIER);
-        }
-    }
-
-    @Override
-    public void onUnequip(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        EntityAttributeInstance flightAttributeInstance = entity.getAttributeInstance(CaelusApi.getInstance().getFlightAttribute());
-        if (flightAttributeInstance != null) {
-            flightAttributeInstance.removeModifier(FLIGHT_MODIFIER);
         }
     }
 }
--- src/main/java/com/hidoni/customizableelytrafabric/integration/trinkets/TrinketsIntegration.java
+++ src/main/java/com/hidoni/customizableelytrafabric/integration/trinkets/TrinketsIntegration.java
@@ -1,10 +1,28 @@
 package com.hidoni.customizableelytrafabric.integration.trinkets;
 
 import com.hidoni.customizableelytrafabric.registry.ModItems;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
+import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Pair;
+
+import java.util.List;
 
 public class TrinketsIntegration {
     public static void register_trinkets() {
         TrinketsApi.registerTrinket(ModItems.CUSTOMIZABLE_ELYTRA, new CustomizableElytraTrinket());
+        EntityElytraEvents.CUSTOM.register((entity, tickElytra) -> {
+            TrinketComponent trinketComponent = TrinketsApi.getTrinketComponent(entity).orElse(null);
+            if (trinketComponent != null) {
+                List<Pair<SlotReference, ItemStack>> equipped = trinketComponent.getEquipped(ModItems.CUSTOMIZABLE_ELYTRA);
+                if (!equipped.isEmpty()) {
+                    Pair<SlotReference, ItemStack> slotReferenceItemStackPair = equipped.get(0);
+                    return CustomizableElytraTrinket.useCustomElytraTrinket(entity, slotReferenceItemStackPair, tickElytra);
+                }
+            }
+            return false;
+        });
     }
 }

5a0879f1a83644e01c4a05c9641ad17ba3cb5df4
Hidoni <ron30086@walla.com>
2021-12-01 23:59:26 +0200

    Backport elytra color blending fix

---
+++ src/main/java/com/hidoni/customizableelytrafabric/mixin/DyeableItemMixin.java
@@ -0,0 +1,22 @@
+package com.hidoni.customizableelytrafabric.mixin;
+
+import com.hidoni.customizableelytrafabric.item.CustomizableElytraItem;
+import com.hidoni.customizableelytrafabric.item.ElytraWingItem;
+import net.minecraft.item.DyeableItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(DyeableItem.class)
+public interface DyeableItemMixin {
+    @Redirect(method = "blendAndSetColor(Lnet/minecraft/item/ItemStack;Ljava/util/List;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/DyeableItem;hasColor(Lnet/minecraft/item/ItemStack;)Z"))
+    private static boolean redirectHasColor(DyeableItem instance, ItemStack stack) {
+        if (instance instanceof CustomizableElytraItem || instance instanceof ElytraWingItem) {
+            NbtCompound nbtCompound = stack.getSubTag("display");
+            return nbtCompound != null && nbtCompound.contains("color", 99);
+        }
+        return instance.hasColor(stack);
+    }
+}
